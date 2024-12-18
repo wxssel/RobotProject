119bcb055eaf668e5e72a2443787207fa88cc188
Wessel van Dijk <w.vandijk3@student.avans.nl>
2024-12-18 12:39:49 +0100

    test

--- obj/Debug/net8.0/RobotProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/RobotProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RobotProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5db7a5d1d798758561d53d096f0fec9f9e60a52")]
 [assembly: System.Reflection.AssemblyProductAttribute("RobotProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RobotProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
